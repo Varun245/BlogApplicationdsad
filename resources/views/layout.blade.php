cc67f593b2795f3fdafed469dc8592b75b78a9dc
Varun Ransubhe <varunransubhe12@gmail.com>
2019-01-18 10:32:19 +0530

    Blogs Comments task

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,9 +18,6 @@
 
   <title>Blogs!!</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
   <!-- Custom styles for this template -->
   <link href="<?php echo asset('css/sticky-footer-navbar.css')?>" rel="stylesheet">
   <link href="<?php echo asset('css/pagination.css')?>" rel="stylesheet">
@@ -82,17 +79,6 @@
       <span class="text-muted"></span>
     </div>
   </footer>
-
-  <!-- Bootstrap core JavaScript
-    ================================================== -->
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-  <script>
-    window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
-  </script>
-  <script src="../../assets/js/vendor/popper.min.js"></script>
-  <script src="../../dist/js/bootstrap.min.js"></script>
 
 </body>
 
